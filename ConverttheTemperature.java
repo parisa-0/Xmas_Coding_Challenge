abe5e6130264c69c5e1d9fdf3c809fe1d1216e10
Parisa <134794618+parisa-0@users.noreply.github.com>
2023-12-04 19:33:20 +0000

    Update ConverttheTemperature.java

--- ConverttheTemperature.java
+++ ConverttheTemperature.java
@@ -1,9 +1,6 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double[] result = new double[2];
-
-        result[0] = celsius + 273.15;
-        result[1] = celsius * 1.80 + 32.00;
+        double[] result = {celsius + 273.15,celsius * 1.80 + 32.00};
         
         return result;
     }
