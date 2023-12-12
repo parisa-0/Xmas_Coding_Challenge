b737bb7921c7b83873d8b130bc937800d03c5cfb
Parisa <134794618+parisa-0@users.noreply.github.com>
2023-12-12 14:30:22 +0000

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+//https://www.youtube.com/watch?v=9kcZE8esf7Q
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        int [][] dp = new int[triangle.size() + 1][triangle.size() + 1];
+
+        for(int i = triangle.size() - 1; i >=0 ; i--) {
+            for(int j = 0; j <= i; j++) {
+              dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+}
